2b98278f40fc60dc78b93519590e8643566910e7
Kirill <horushevskiykirill@gmail.com>
2023-08-19 15:59:52 +0300

    commit 3

--- components/CookieModal/CookieModal.tsx
+++ components/CookieModal/CookieModal.tsx
@@ -14,15 +14,15 @@ const font = Open_Sans({
 interface ICookieModal {}
 
 const CookieModal: FC = ({}) => {
-  const statusCoolieModal = sessionStorage.getItem("CookieModal");
-  const [isActive, setActive] = useState(!statusCoolieModal);
-  const CloseModal = () => {
-    setActive(false);
-    sessionStorage.setItem("CookieModal", JSON.stringify(isActive));
-  };
+  // const statusCoolieModal = sessionStorage.getItem("CookieModal");
+  // const [isActive, setActive] = useState(!statusCoolieModal);
+  // const CloseModal = () => {
+  //   setActive(false);
+  //   sessionStorage.setItem("CookieModal", JSON.stringify(isActive));
+  // };
   return (
     <>
-      {isActive === true && (
+      {/* {isActive === true && (
         <section className={`${s.cookieModal} bg-white text-green`}>
           <div className="container">
             <section>
@@ -49,7 +49,33 @@ const CookieModal: FC = ({}) => {
             </section>
           </div>
         </section>
-      )}
+      )} */}
+      <section className={`${s.cookieModal} bg-white text-green`}>
+        <div className="container">
+          <section>
+            <p style={font.style}>
+              This site uses cookies for your convenience.
+              <Link
+                style={font.style}
+                className="text-black "
+                target="_blank"
+                href={"https://policies.google.com/technologies/cookies"}
+              >
+                Learn more
+              </Link>
+            </p>
+            <div>
+              <button
+                // onClick={CloseModal}
+                style={font.style}
+                className="bg-green text-white hover:bg-black hover:text-white"
+              >
+                Ok, got it
+              </button>
+            </div>
+          </section>
+        </div>
+      </section>
     </>
   );
 };
--- components/InfoBar/InfoBar.tsx
+++ components/InfoBar/InfoBar.tsx
@@ -17,16 +17,16 @@ const font = Open_Sans({
 interface IInfoBar {}
 
 const InfoBar: FC = ({}) => {
-  const statusInfoBar = sessionStorage.getItem("infoBar");
-  const [isInfoBar, setInfoBar] = useState(!statusInfoBar);
-  // Close bar
-  const CloseBar = () => {
-    setInfoBar(false);
-    sessionStorage.setItem("infoBar", JSON.stringify(isInfoBar));
-  };
+  // const statusInfoBar = sessionStorage.getItem("infoBar");
+  // const [isInfoBar, setInfoBar] = useState(!statusInfoBar);
+  // // Close bar
+  // const CloseBar = () => {
+  //   setInfoBar(false);
+  //   sessionStorage.setItem("infoBar", JSON.stringify(isInfoBar));
+  // };
   return (
     <>
-      {isInfoBar === true && (
+      {/* {isInfoBar === true && (
         <section className={`${s.infoBar} bg-white text-black`}>
           <div className="container">
             <section style={font.style} className={s.infoBar_inner}>
@@ -54,7 +54,36 @@ const InfoBar: FC = ({}) => {
             </section>
           </div>
         </section>
-      )}
+      )} */}
+      <section className={`${s.infoBar} bg-white text-black`}>
+        <div className="container">
+          <section style={font.style} className={s.infoBar_inner}>
+            <div>
+              <p className="text-green">
+                <BiSupport />
+              </p>
+              <p>Support</p>
+              <p>(406) 555-555</p>
+            </div>
+            <div>
+              <p>
+                Sign up and GET 20% OFF for your first order.
+                <Link className="text-green" href="">
+                  Sign up now
+                </Link>
+              </p>
+            </div>
+            <button
+            // onClick={CloseBar}
+            >
+              <p className="text-black ">
+                {" "}
+                <IoClose />
+              </p>
+            </button>
+          </section>
+        </div>
+      </section>
     </>
   );
 };
